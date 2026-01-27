4f9a5fc080bb7e0a953815dea0225630a926abed
KhanhNguyenVimaru <khanhnd05@gmail.com>
2026-01-28 05:59:57 +0700

    refactor(ui): - Revise overall UI layout - Merge Parameter Trends (no longer a separate component) - Update env-analysis-project version

--- env-analysis-project/Controllers/HomeController.cs
+++ env-analysis-project/Controllers/HomeController.cs
@@ -1,21 +1,52 @@
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using env_analysis_project.Data;
 using env_analysis_project.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace env_analysis_project.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly env_analysis_projectContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, env_analysis_projectContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var emissionSources = await _context.EmissionSource
+                .Where(s => !s.IsDeleted)
+                .OrderBy(s => s.SourceName)
+                .Select(s => new
+                {
+                    Id = s.EmissionSourceID,
+                    Label = s.SourceName
+                })
+                .ToListAsync();
+
+            var parameters = await _context.Parameter
+                .Where(p => !p.IsDeleted)
+                .OrderBy(p => p.ParameterName)
+                .Select(p => new
+                {
+                    Code = p.ParameterCode,
+                    Label = p.ParameterName,
+                    Unit = p.Unit,
+                    Type = ParameterTypeHelper.Normalize(p.Type)
+                })
+                .ToListAsync();
+
+            ViewBag.EmissionSources = emissionSources;
+            ViewBag.Parameters = parameters;
+
             return View();
         }
 
